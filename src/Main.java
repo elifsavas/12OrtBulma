b249dcb57097eea8cd005187f0ee3fdea708a57d
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-30 22:47:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int i, k, x = 0, total = 0;
+        double ort;
+
+        x = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Sayı Giriniz: ");
+        k = input.nextInt();
+
+        for (i = 1; i <= k; i++) {
+            if ((i % 3 == 0) && (i % 4 == 0)) {
+
+                total += i;
+                x++;
+
+            }
+        }
+
+        System.out.println(total / x);
     }
+
 }
